d0ec1a5a1fbb20d11c30ddb908dc88ff83c27cda
kimsejin <kimsejin2118@gmail.com>
2023-11-30 17:43:19 +0900

    servlet (템플릿 없이) html 동적으로 다루기

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+//    MemberRepository memberRepository = new MemberRepository();   -> singleton이라 불가능
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    //test가 하나가 아니기 때문에 항상 하나의 테스트 실행 이후 초기화 필수
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);   //orgj
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2); //Assert를 option + enter로 static으로 포함
+        assertThat(result).contains(member1, member2);
+    }
+}
