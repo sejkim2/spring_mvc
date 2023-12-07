c54804801c4e8549cabefbe08647198fbc5fcc17
kimsejin <kimsejin2118@gmail.com>
2023-12-07 17:11:14 +0900

    servlet 종속성 제거(v3)

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+
+        //controller -> view로 이동할 때 사용(제어권이 view로 넘겨짐)
+        //정확히는 다른 서블릿이나 jsp로 이동할 수 있는 기능 (서버 내부에서 다시 호출 발생)
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,32 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username"); //getParameter : Get or postform에서 가져옴
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", member); //request 내부에 있는 Map에 (key, value) 형태로 저장
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
